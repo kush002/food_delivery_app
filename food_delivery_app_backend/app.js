ca2d578198ae90d8058677263269c7fd842f9b3d
Kushagra Singh <kushagrasingh543@gmail.com>
2023-05-29 22:08:13 +0530

    updating app

--- food_delivery_app_backend/app.js
+++ food_delivery_app_backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./.env" });
 const bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
