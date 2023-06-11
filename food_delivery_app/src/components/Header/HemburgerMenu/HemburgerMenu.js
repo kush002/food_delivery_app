d47ad1ed57c00e33a912bf52853d164e1e6b9f7c
Kushagra Singh <kushagrasingh543@gmail.com>
2023-06-11 23:18:57 +0530

    hemburger menu final

--- food_delivery_app/src/components/Header/HemburgerMenu/HemburgerMenu.js
+++ food_delivery_app/src/components/Header/HemburgerMenu/HemburgerMenu.js
@@ -7,8 +7,10 @@ import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { cartActions } from "../../../store/cart-slice";
+import { getToken } from "../../../util/user";
 
 const HemburgerMenu = ({ onClick }) => {
+  const token = getToken();
   const dispatch = useDispatch();
   const onLogoutHandler = () => {
     dispatch(
@@ -20,7 +22,7 @@ const HemburgerMenu = ({ onClick }) => {
 
     localStorage.removeItem("token");
     localStorage.removeItem("expiration");
-    return redirect("/");
+    return redirect("/login");
   };
   return (
     <Modal class={classes.modal} onClick={onClick}>
@@ -31,29 +33,41 @@ const HemburgerMenu = ({ onClick }) => {
           style={{ color: "var(--buttonbg-color)" }}
           onClick={onClick}
         />
-        <ul className={classes.list}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-          <li>
-            <Link to="/admin-page">Admin</Link>
-          </li>
-          <li>
-            <Link to="/orders">Cart</Link>
-          </li>
-          <li>
-            <Link to="/account/address">My Account</Link>
-          </li>
-          <li>
-            <Link onClick={onLogoutHandler}>Logout</Link>
-          </li>
-        </ul>
+        {token && (
+          <ul className={classes.list} onClick={onClick}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+            <li>
+              <Link to="/admin-page">Admin</Link>
+            </li>
+            <li>
+              <Link to="/orders">Cart</Link>
+            </li>
+            <li>
+              <Link to="/account/address">My Account</Link>
+            </li>
+            <li>
+              <Link onClick={onLogoutHandler}>Logout</Link>
+            </li>
+          </ul>
+        )}
+        {!token && (
+          <ul className={classes.list} onClick={onClick}>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+        )}
       </div>
     </Modal>
   );
